db497d3a3f0de2a9a8d63c4497920cae2b6008e0
maulana-tech <firdaussyah03@gmail.com>
2025-12-11 20:04:20 +0700

    modify:title

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Barang - Sistem Manajemen Inventaris Modern</title>
+    <title>Tugas Akhir - Proyek Web Laravel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
